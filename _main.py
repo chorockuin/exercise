b2c1beae25aea84c5f0605188d200f0fd3ed01cc
chorockuin <chorockuin@lginnotek.com>
2021-12-08 14:34:18 +0900

    20211208

--- _main.py
+++ _main.py
@@ -21,4 +21,69 @@ def _41():
             else:
                 return v
 
-_41()
+def _42():
+    class A:
+        def __init__(self):
+            self.public_var = 1
+            self._protected_var = 2
+            self.__private_var = 3
+            print(self.public_var, self._protected_var, self.__private_var)
+    
+    class B(A):
+        def access(self):
+            print(self.__dict__)
+            print(self.public_var, self._protected_var, self.__private_var)
+
+    b = B()
+    b.access()
+
+from weakref import WeakKeyDictionary
+def _46():
+    class Homework:
+        def __init__(self):
+            self._grade = 0
+
+        @property
+        def grade(self):
+            return self._grade
+
+        @grade.setter
+        def grade(self, value):
+            if not (0 <= value <= 100):
+                raise ValueError('점수는 0과 100 사이입니다')
+            self._grade = value
+
+    galileo = Homework()
+    galileo.grade = 100
+    print(galileo.grade)
+
+    class Grade:
+        def __init__(self):
+            self._values = WeakKeyDictionary()
+
+        def __get__(self, instance, instance_type):
+            if instance is None:
+                return self
+            return self._values.get(instance, 0)
+
+        def __set__(self, instance, value):
+            if not (0 <= value <= 100):
+                raise ValueError('점수는 0과 100 사이입니다')
+            self._values[instance] = value
+
+    class Exam:
+        math_grade = Grade()
+        writing_grade = Grade()
+        science_grade = Grade()
+
+    exam = Exam()
+    exam.writing_grade = 40
+    print(exam.writing_grade)
+
+    exam2 = Exam()
+    exam2.writing_grade = 80
+    print(exam2.writing_grade)
+
+_46()
+# _42()
+# _41()
